5b423016fa7c97317440e3284e58f98f5ac3e6fa
Lakshmi Prasanna <lakshmip@thoughtworks.com>
2017-08-09 19:30:44 +0530

    Create daimond with asterisk

---
+++ src/DaimondTriangle.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class DaimondTriangle {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter number : ");
+       int number=scanner.nextInt();
+        for(int i=1;i<=number;i++){
+            printSpace(number-i);
+            printStar(2*i-1);
+            printSpace(number-i);
+            System.out.println();
+        }
+        for(int i=number-1;i>=1;i--){
+            printSpace(number-i);
+            printStar(2*i-1);
+            printSpace(number-i);
+            System.out.println();
+        }
+    }
+
+    private static void printStar(int num) {
+        for(int i=1;i<=num;i++){
+            System.out.print("*");
+        }
+    }
+
+    private static void printSpace(int num) {
+        for(int i=1;i<=num;i++){
+            System.out.print(" ");
+        }
+    }
+}
